9ee1cea635691f677456fc2f61539ea6db244aba
AJ ONeal <coolaj86@gmail.com>
2014-01-17 20:05:34 -0700

    reformatted

--- test-rrule.js
+++ test-rrule.js
@@ -1,84 +1,86 @@
-'use strict';
+(function () {
+  'use strict';
 
-var recur = require('./recur').Recur
-  , RRule = require('rrule').RRule
-  , moment = require('moment')
-  , ruleObj
-  , rfcString
-  , rule
-  , m = moment() // new Date()
-  , before
-  , current
-  , current2
-  , next
-  ;
-
-ruleObj = {
-  freq: 'weekly'
-, interval: '2'
-, byday: [ 'tu', 'su' ]
-, wkst: 'su'
-};
-
-if (ruleObj.count) {
-  delete ruleObj.count;
-}
-
-function getDtStart(ruleObj, method) {
-  var doubleInt
-    , d = moment(m)
+  var recur = require('./recur').Recur
+    , RRule = require('rrule').RRule
+    , moment = require('moment')
+    , ruleObj
+    , rfcString
+    , rule
+    , m = moment() // new Date()
+    , before
+    , current
+    , current2
+    , next
     ;
 
-  doubleInt = (ruleObj.interval || 1) * 2;
+  ruleObj = {
+    freq: 'weekly'
+  , interval: '2'
+  , byday: [ 'tu', 'su' ]
+  , wkst: 'su'
+  };
 
-  switch(ruleObj.freq) {
-  case 'yearly':
-    d[method](doubleInt, 'years');
-    break;
-  case 'monthly':
-    d[method](doubleInt, 'months');
-    break;
-  case 'weekly':
-    d[method](doubleInt, 'weeks');
-    break;
-  case 'daily':
-    d[method](doubleInt, 'days');
-    break;
-  case 'hourly':
-    d[method](doubleInt, 'hours');
-    break;
-  case 'minutely':
-    d[method](doubleInt, 'minutes');
-    break;
-  case 'secondly':
-    d[method](doubleInt, 'seconds');
-    break;
+  if (ruleObj.count) {
+    delete ruleObj.count;
   }
 
-  return d.toDate().toISOString();
-}
+  function getDtStart(ruleObj, method) {
+    var doubleInt
+      , d = moment(m)
+      ;
 
-if (!ruleObj.dtstart) {
-  ruleObj.dtstart = getDtStart(ruleObj, 'subtract');
-}
-if (!ruleObj.until) {
-  ruleObj.until = getDtStart(ruleObj, 'add');
-}
+    doubleInt = (ruleObj.interval || 1) * 2;
 
-rfcString = recur.stringify(ruleObj);
+    switch(ruleObj.freq) {
+    case 'yearly':
+      d[method](doubleInt, 'years');
+      break;
+    case 'monthly':
+      d[method](doubleInt, 'months');
+      break;
+    case 'weekly':
+      d[method](doubleInt, 'weeks');
+      break;
+    case 'daily':
+      d[method](doubleInt, 'days');
+      break;
+    case 'hourly':
+      d[method](doubleInt, 'hours');
+      break;
+    case 'minutely':
+      d[method](doubleInt, 'minutes');
+      break;
+    case 'secondly':
+      d[method](doubleInt, 'seconds');
+      break;
+    }
 
-console.log(rfcString);
-rule = RRule.fromString(rfcString);
-before = rule.before(m.toDate());
-current = rule.after(m.toDate(), true);
-current2 = rule.after(m.toDate());
+    return d.toDate().toISOString();
+  }
 
-if (current.toISOString() === current2.toISOString()) {
-  next = rule.after(current);
-} else {
-  next = current2;
-}
+  if (!ruleObj.dtstart) {
+    ruleObj.dtstart = getDtStart(ruleObj, 'subtract');
+  }
+  if (!ruleObj.until) {
+    ruleObj.until = getDtStart(ruleObj, 'add');
+  }
 
-console.log('Previous:', before);
-console.log('Current:', current);
-console.log('Next:', next);
+  rfcString = recur.stringify(ruleObj);
+
+  console.log(rfcString);
+  rule = RRule.fromString(rfcString);
+  before = rule.before(m.toDate());
+  current = rule.after(m.toDate(), true);
+  current2 = rule.after(m.toDate());
+
+  if (current.toISOString() === current2.toISOString()) {
+    next = rule.after(current);
+  } else {
+    next = current2;
+  }
+
+  console.log('Previous:', before);
+  console.log('Current:', current);
+  console.log('Next:', next);
+}());
